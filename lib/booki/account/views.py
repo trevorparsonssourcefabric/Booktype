24baba50a09a5414c78be5c7adc75e528fb08a0f
Aleksandar Erkalovic <aerkalov@gmail.com>
2012-10-09 16:25:44 +0200

    Open TOC by default and don't create dummy chapter anymore

--- lib/booki/account/views.py
+++ lib/booki/account/views.py
@@ -618,15 +618,15 @@ def create_book(request, username):
 
             now = datetime.datetime.now()
 
-            chapter = models.Chapter(book = book,
-                                     version = book.version,
-                                     url_title = 'sample-chapter',
-                                     title = 'Sample chapter',
-                                     status = stat,
-                                     content = '<h1>Sample chapter</h1><p></p>',
-                                     created = now,
-                                     modified = now)
-            chapter.save()
+#            chapter = models.Chapter(book = book,
+#                                     version = book.version,
+#                                     url_title = 'sample-chapter',
+#                                     title = 'Sample chapter',
+#                                     status = stat,
+#                                     content = '<h1>Sample chapter</h1><p></p>',
+#                                     created = now,
+#                                     modified = now)
+#            chapter.save()
 
             
             from django.core.files import File
